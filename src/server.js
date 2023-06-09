9f103b5fdb84fd2b18b0f841f1e61a7e12000d1f
matheusvsm <matheusmoura.eng.eletrica@gmail.com>
2023-06-09 03:00:55 -0300

    corrigindo

--- src/server.js
+++ src/server.js
@@ -15,4 +15,4 @@ app.get("/home", (req, res) => {
 app.listen({
     host: '0.0.0.0',
     port: process.env.PORT ? Number(process.env.PORT) : 3333
-    }).then(() => { console.log('Server Running') })
+    })
